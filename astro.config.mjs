e09716d292217463c683cbf852e919c29e274b6e
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-20 01:14:05 -0800

    Style: Finshed coding header.
    Turned on "prefetchAll" links, added a font for Title, added some hover animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
-});
+	integrations: [tailwind(), alpinejs()],
+	prefetch: {
+		prefetchAll: true
+	}
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				karla: ['Karla Variable', 'sans-serif']
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 }
