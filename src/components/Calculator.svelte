45ab163a96456177b395b4ae589ed41f8c020776
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-24 00:12:04 -0800

    Feat: finished it!!!!!

--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -1,6 +1,10 @@
 <script>
 	import metals from '../api/metals.json'
+	import currencies from '../api/currencies.json'
 	let amount = '19'
+	let metal = 'Gold'
+	let currency = '$ USD'
+	let answer = 0
 
 	function handleAmountInputNumbers(event) {
 		const value = event.target.value
@@ -8,6 +12,17 @@
 		if (value !== validValue) {
 			amount = validValue
 		}
+	}
+
+	function doMath() {
+		const lastLetters = currency.slice(-3)
+		const metalValue = metals.metals[metal.toLowerCase().trim()]
+		const currencyValue = metals.currencies[lastLetters.toUpperCase().trim()]
+		const calculation = amount * metalValue * currencyValue
+		answer = calculation
+			.toFixed(2)
+			.toString()
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 	}
 </script>
 
@@ -19,6 +34,7 @@
 		type="text"
 		bind:value={amount}
 		on:input={handleAmountInputNumbers}
+		on:input={doMath}
 		class="appearance-none flex w-28 text-center border-b-4 pt-1 text-gray-900 focus:border-green-500 focus:ring-green-500 dark:bg-zinc-800 dark:text-green-50 dark:outline-none dark:outline-offset-1 dark:border-green-800"
 		id="amount"
 		name="amount"
@@ -31,8 +47,11 @@
 		class="appearance-none w-52 text-center border-b-4 pt-1 mx-4 text-gray-900 focus:border-green-500 focus:ring-green-500 dark:bg-zinc-800 dark:text-green-50 dark:outline-none dark:outline-offset-1 dark:border-green-800"
 		list="metals"
 		required
-		value="Gold"
-		on:input={(e) => (e.target.style.width = e.target.value.length + 1 + 'ch')}
+		bind:value={metal}
+		on:input={doMath}
+		on:input={(e) =>
+			(e.target.style.width =
+				e.target.value.length + 1 < 5 ? '5ch' : e.target.value.length + 1 + 'ch')}
 	/>
 	<datalist id="metals">
 		<option class="text-l" value="Gold"> </option>
@@ -45,21 +64,23 @@
 		class="appearance-none w-72 border-b-4 pt-1 mx-4 text-center text-gray-900 focus:border-green-500 focus:ring-green-500 dark:bg-zinc-800 dark:text-green-50 dark:outline-none dark:outline-offset-1 dark:border-green-800"
 		list="curr"
 		required
-		value="$ USD"
-		on:input={(e) => (e.target.style.width = e.target.value.length + 1 + 'ch')}
+		bind:value={currency}
+		on:input={doMath}
+		on:input={(e) =>
+			(e.target.style.width =
+				e.target.value.length + 1 < 5 ? '5ch' : e.target.value.length + 1 + 'ch')}
 	/>
 	<datalist id="curr">
-		<option class="text-l" value="$ USD"> </option>
-		<option class="text-l" value="$ CAD"> </option>
-		<option class="text-l" value="¥ YEN "> </option>
+		{#each Object.values(currencies) as c}
+			<option class="text-l" value="{c.symbol_native} {c.code}"> </option>
+		{/each}
 	</datalist>
 	is:
 </form>
 <br />
-<div class="flex items-center justify-center pt-36 text-5xl text-green-50 sm:text-7xl md:text-9xl">
-	{metals.metals.gold}
-	{#if amount !== null}
-		{amount}
+<div class="flex items-center justify-center pt-36 text-5xl text-green-300 sm:text-7xl md:text-9xl">
+	{#if answer !== null}
+		{answer}
 	{/if}
 </div>
 
