b56d2b84b87c207911fb49ad327d62715f5f38e9
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-26 18:11:21 -0800

    Fix: still trying to fix font not showing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import netlify from '@astrojs/netlify'
+import { resolve } from 'path'
 
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		resolve: {
+			alias: {
+				$fonts: resolve('public/fonts'),
+			},
+		},
+	},
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
