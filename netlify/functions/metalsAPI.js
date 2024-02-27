e0166bed8b376bbd9b8a23a262b369bc0a5c9509
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-26 19:04:22 -0800

    Fix: added console log for when api key didn't work

--- netlify/functions/metalsAPI.js
+++ netlify/functions/metalsAPI.js
@@ -1,5 +1,9 @@
-const APIKEY = import.meta.env.METALDEV_API.toString()
+const APIKEY = process.env.METALDEV_API.toString()
 const url = 'https://api.metals.dev/v1/latest?api_key=' + APIKEY + '&currency=USD&unit=toz'
+
+if (!APIKEY) {
+	throw new Error('METALDEV_API environment variable is not set!!! -nima')
+}
 
 async function fetchMetalRates() {
 	const response = await fetch(url, {
