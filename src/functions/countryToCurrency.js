c3dac689a85a71eeae15ef8a624535ee51231ee3
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-03-13 15:08:17 -0700

    Fix: if country is not found in function, default to USD
    I forgot that there was this continent called EUROPE and that they had the same currency, so I have to change my logic soon. tbd

--- src/functions/countryToCurrency.js
+++ src/functions/countryToCurrency.js
@@ -1,8 +1,13 @@
 import currencyData from '../api/currencies.json'
 
 export function countryToCurrency(countryCode) {
+	let printedCurr = ''
 	let matchedCurrency = Object.values(currencyData).find((c) => c.code.slice(0, 2) === countryCode)
-	let printedCurr = `${matchedCurrency.symbol_native} ${matchedCurrency.code}`
+	if (!matchedCurrency) {
+		printedCurr = '$ USD'
+	} else {
+		printedCurr = `${matchedCurrency.symbol_native} ${matchedCurrency.code}`
+	}
 
 	return printedCurr
 }
